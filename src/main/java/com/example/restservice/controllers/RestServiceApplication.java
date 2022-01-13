d5a7d10fd06c84f6b26a5f557739ceebd9830de4
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2022-01-12 23:12:35 -0800

    Fix "bean not found" issue

--- src/main/java/com/example/restservice/controllers/RestServiceApplication.java
+++ src/main/java/com/example/restservice/controllers/RestServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.restservice.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.example.storage")
+@EntityScan("com.example.storage.entities")
 public class RestServiceApplication {
     public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
